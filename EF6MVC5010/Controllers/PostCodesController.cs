b247d09a28841df1901e6131f184a393c5cf9765
pkirklewski <kirklewskis@gmail.com>
2017-03-22 20:14:54 +0000

    first commit

---
+++ EF6MVC5010/Controllers/PostCodesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using EF6MVC5010.Models;
+
+namespace EF6MVC5010.Controllers
+{
+    public class PostCodesController : Controller
+    {
+        private UKPostCodesEntities db = new UKPostCodesEntities();
+
+        // GET: PostCodes
+        public ActionResult Index()
+        {
+            return View(db.PostCodes.ToList());
+        }
+
+        // GET: PostCodes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PostCode postCode = db.PostCodes.Find(id);
+            if (postCode == null)
+            {
+                return HttpNotFound();
+            }
+            return View(postCode);
+        }
+
+        // GET: PostCodes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PostCodes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "pID,postcode,latitude,longitude")] PostCode postCode)
+        {
+            if (ModelState.IsValid)
+            {
+                db.PostCodes.Add(postCode);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(postCode);
+        }
+
+        // GET: PostCodes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PostCode postCode = db.PostCodes.Find(id);
+            if (postCode == null)
+            {
+                return HttpNotFound();
+            }
+            return View(postCode);
+        }
+
+        // POST: PostCodes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "pID,postcode,latitude,longitude")] PostCode postCode)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(postCode).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(postCode);
+        }
+
+        // GET: PostCodes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PostCode postCode = db.PostCodes.Find(id);
+            if (postCode == null)
+            {
+                return HttpNotFound();
+            }
+            return View(postCode);
+        }
+
+        // POST: PostCodes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            PostCode postCode = db.PostCodes.Find(id);
+            db.PostCodes.Remove(postCode);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ EF6MVC5010/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EF6MVC5010.MvcApplication" Language="C#" %>
